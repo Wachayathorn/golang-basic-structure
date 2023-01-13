3b35c6b3a9183455f028d6031524f7d903d0e228
wachayathorn.s <wachayathorn@gmail.com>
2023-01-13 22:13:03 +0700

    add config & add logrus

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+	"github.com/wachayathorn/golang-basic-structure/pkg/config"
 	"github.com/wachayathorn/golang-basic-structure/pkg/handler"
 	"github.com/wachayathorn/golang-basic-structure/pkg/service"
 )
 
 func main() {
-	r := gin.Default()
+	var env string
+	flag.StringVar(&env, "env", "local", "Environment")
+	flag.Parse()
+	c := config.Config{}
+	port, err := c.Load(env)
+	if err != nil {
+		log.Fatalf("Load config error:%s", err.Error())
+	}
 
+	log.SetFormatter(&log.TextFormatter{})
+
+	r := gin.Default()
 	svc := service.InitService()
 	handler.InitHandler(r, svc)
 
+	log.Infof("Server start port:%s\n", port)
 	r.Run()
 }
--- pkg/config/load.go
+++ pkg/config/load.go
@@ -1,1 +1,56 @@
 package config
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/spf13/viper"
+)
+
+type ConfigInterface interface {
+	Load(env string) error
+}
+
+type Config struct {
+	Env  string `mapstructure:"env"`
+	Port string `mapstructure:"port"`
+}
+
+type State string
+
+const (
+	StateLocal State = "local"
+	StateDev   State = "dev"
+)
+
+func (c *Config) ParseState(env string) State {
+	switch env {
+	case "dev":
+		return StateDev
+	default:
+		return StateLocal
+	}
+}
+
+func (c *Config) Load(env string) (string, error) {
+	vn := viper.New()
+	vn.AddConfigPath(filepath.Join(".", "pkg", "config", "resource"))     // Set path to load config.yml
+	vn.SetConfigName(fmt.Sprintf("config.%s", c.ParseState(string(env)))) // Load config file
+
+	if err := vn.ReadInConfig(); err != nil { // Read config from file
+		return "", err
+	}
+
+	if err := c.binding(vn); err != nil {
+		return "", err
+	}
+
+	return c.Port, nil
+}
+
+func (c *Config) binding(vn *viper.Viper) error {
+	if err := vn.Unmarshal(&c); err != nil {
+		return err
+	}
+	return nil
+}
